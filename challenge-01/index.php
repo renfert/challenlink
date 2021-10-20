97fa6bda6b5ff94c904a0edce8eb922a0e10cd1b
Renato Fernando <tin22909@gmail.com>
2021-10-20 15:37:59 -0500

    challenge1 complete

--- challenge-01/index.php
+++ challenge-01/index.php
@@ -1,10 +1,19 @@
 <?php
 
-function findPoint($strArr)
-{
-    // code goes here
-    return $strArr;
+function findPoint($strArr) { 
+
+    $arr1 = explode(", ", $strArr[0]);
+    $arr2 = explode(", ", $strArr[1]);
+
+    $result = [];
+
+    foreach ($arr1 as $number) {
+      if (in_array($number, $arr2)) {
+        array_push($result,$number);
+      }
+    }
+    return empty($result) ? "false" : implode(",", $result);
 }
 
-// keep this function call here
 echo findPoint(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']);
+?>
