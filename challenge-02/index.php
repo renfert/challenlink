6ea1d897f4df5d3e7e1b1102133e50e4c2e1d5f1
Renato Fernando <tin22909@gmail.com>
2021-10-20 15:36:15 -0500

    challenge2 complete

--- challenge-02/index.php
+++ challenge-02/index.php
@@ -1,10 +1,63 @@
 <?php
 
+
 function noIterate($strArr)
 {
-    // code goes here
-    return $strArr;
+    $phrase = $strArr[0];
+    $chars = $strArr[1];
+    $maxLength = strlen($phrase);
+    if (!subStrWithAllChars($chars, $phrase)) {
+        return '';
+    }
+    $result = $phrase;
+    for ($i = $maxLength - 1; $i > strlen($chars); $i--) {
+        for ($j = 0; $j <= $maxLength - $i + 1; $j++) {
+            $window = substr($phrase, $j, $i);
+            if (subStrWithAllChars($chars, $window)) {
+                $result = $window;
+            }
+        }
+
+    }
+
+
+    return $result;
 }
 
-// keep this function call here
-echo noIterate(["ahffaksfajeeubsne", "jefaa"]);
+function subStrWithAllChars($search, $phrase)
+{
+    $arraySearch = str_split($search);
+    $arrayphrase = str_split($phrase);
+    foreach ($arraySearch as $item) {
+        $index = array_search($item, $arrayphrase);
+        if ($index !== false) {
+            unset($arrayphrase[$index]);
+            $arrayphrase = array_values($arrayphrase);
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+   
+echo noIterate(["ahffaksfajeeubsne", "jefaa"]);  //aksfaje
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
